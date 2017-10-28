54d12ff80e3c23beb30bdad868888509b4bb2835
Evseev Nikolay <1312743+evseevnn@users.noreply.github.com>
2017-10-28 17:29:46 +0700

    Fixes for nuxt@latest

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
+const { Builder } = require('nuxt');
 
 module.exports = app => {
   app.config.coreMiddleware.splice(0, 0, 'nuxtRender');
+  const NuxtBuilder = new Builder(app.nuxt);
   app.beforeStart(function* () {
     try {
-      yield app.nuxt.build();
+      yield NuxtBuilder.build();
       if (!app.config.nuxt.dev) {
         console.log('[nuxt] Building done'); // eslint-disable-line no-console
       }
